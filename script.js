781cc0907bc97cfa52d3b92abe9a4d8440117b27
joaogabriel2104cdk <joao.estefani@escola.pr.gov.br>
2025-08-01 08:01:04 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+var botao = document.createElement("button");
+botao.innerHTML = "Explorar Destinos";
+botao.onclick = function() {
+    alert(Redirecionando);
+};
+botao.style.padding = "10px 20px";
+botao.style.fontSize = "16px";
+botao.style.backgroundColor = "4CAF50";
+botao.style.color = "white";
+botao.style.border = "1px solid black";
+botao.style.borderRadius = "2px";
+botao.style.cursor = "pointer";
+document.body.appendChild(botao);
